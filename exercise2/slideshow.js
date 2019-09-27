ed328c6571ff0f86de0b26ff5810419afac90d8e
Archana Ramakrishnan <archana2359@gmail.com>
2019-09-27 15:37:06 -0500

    feat: added an array containing all the image names

--- exercise2/slideshow.js
+++ exercise2/slideshow.js
@@ -1,7 +1,19 @@
 //pictures source: https://www.pexels.com/royalty-free-images/
+var pictureNames=["delicious-dish-food-2664216.jpg",
+"cheese-delicious-dinner-2232.jpg",
+"bread-delicious-egg-704569.jpg",
+"biriyani-chicken-cooked-1624487.jpg",
+"blur-close-up-dairy-product-376464.jpg",
+"banana-berries-berry-775032.jpg"]
 
 function next() {
-
+  /*var x = document.getElementById("currentImage").src;
+  console.log(x);
+  var i;
+  for (i = 0; i < cars.length; i++)
+  {
+    text += cars[i] + "<br>";
+  } */
 }
 
 function previous() {
