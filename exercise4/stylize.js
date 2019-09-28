092a6a669e85d26c07193881068cff8b17f86191
Archana Ramakrishnan <archana2359@gmail.com>
2019-09-28 13:51:06 -0500

    feat: added get Elements in js file and removed the css file

---
+++ exercise4/stylize.js
@@ -0,0 +1,13 @@
+//Access the tags:
+var borderR = document.getElementById("border-red");
+var borderG = document.getElementById("border-green");
+var borderB = document.getElementById("border-blue");
+var borderWidth = document.getElementById("border-width");
+
+var bgR = document.getElementById("border-red");
+var bgG = document.getElementById("border-green");
+var bgB = document.getElementById("border-blue");
+
+
+//Change the style attribute
+//someTag.style.backgroundColor = "red";
