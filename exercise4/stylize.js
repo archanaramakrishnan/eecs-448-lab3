072ee30832121744aae6da9cc3a16d45cfa82598
Archana Ramakrishnan <archana2359@gmail.com>
2019-09-29 09:57:09 -0500

    feat: body's background color can now be changed. modified syntax of changing bg color and getting the input values in js file

--- exercise4/stylize.js
+++ exercise4/stylize.js
@@ -1,13 +1,17 @@
-//Access the tags:
-var borderR = document.getElementById("border-red");
-var borderG = document.getElementById("border-green");
-var borderB = document.getElementById("border-blue");
-var borderWidth = document.getElementById("border-width");
+function stylize() {
+  //Access the tags:
+  var borderR = document.getElementById("border-red").value;
+  var borderG = document.getElementById("border-green").value;
+  var borderB = document.getElementById("border-blue").value;
+  var borderWidth = document.getElementById("border-width").value;
 
-var bgR = document.getElementById("border-red");
-var bgG = document.getElementById("border-green");
-var bgB = document.getElementById("border-blue");
+  var bgR = document.getElementById("bg-red").value;
+  var bgG = document.getElementById("bg-green").value;
+  var bgB = document.getElementById("bg-blue").value;
 
-
-//Change the style attribute
-//someTag.style.backgroundColor = "red";
+  //Change the style attribute
+  console.log(bgR);
+  console.log(bgG);
+  console.log(bgB);
+  document.body.style.backgroundColor = 'rgb(' + bgR + ',' + bgG + ',' + bgB + ')';
+}
